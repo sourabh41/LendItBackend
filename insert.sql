ef843590419923a19e9ccac5100a31c109e5ee33
Bhavesh Dhingra <bhavesh4898@gmail.com>
2018-11-26 20:30:20 +0530

    Corrected photos in sample data

--- insert.sql
+++ insert.sql
@@ -1,16 +1,18 @@
 insert into users values ('160050009', 'Sourabh Tote', '', 'Room No 173', 'Hostel 2');
-insert into users values ('160050050', 'Akshay Patidar', 'https://www.cse.iitb.ac.in/~akshay/akshay1.jpg', 'Room No 173', 'Hostel 2');
-insert into users values ('160050106', 'Dhruv Jaglan', 'https://www.cse.iitb.ac.in/~dhruvj/profile2.jpg', 'Room No 174', 'Hostel 2');
-insert into users values ('160050108', 'Bhavesh Dhingra', 'https://www.cse.iitb.ac.in/~bhaveshd/images/dp.jpg', 'Room No 174', 'Hostel 2');
+insert into users values ('160050050', 'Akshay Patidar', '/images/dps/akshay.jpg', 'Room No 173', 'Hostel 2');
+insert into users values ('160050106', 'Dhruv Jaglan', '/images/dps/dhruv.jpg', 'Room No 174', 'Hostel 2');
+insert into users values ('160050108', 'Bhavesh Dhingra', '/images/dps/bhavesh.jpg', 'Room No 174', 'Hostel 2');
 
 insert into item (name, type, owner_id, description, available) values ('The Grand Design', 'Book', '160050108', 'A book by Stephen Hawking', true);
 insert into item (name, type, owner_id, description, available) values ('Hyperspace', 'Book', '160050108', 'A book by Michio Kaku on higher dimensions', true);
 insert into item (name, type, owner_id, description, available) values ('Calculator', 'Electronics', '160050009', 'Casio Scientific Calculator with matrix supprt', true);
 insert into item (name, type, owner_id, description, available) values ('Tennis Racket', 'Sports Equipment', '160050050', 'A cool tennis racket in perfect condition', true);
 
-insert into photo values (1, 'https://www.cse.iitb.ac.in/~bhaveshd/share/3.png');
-insert into photo values (1, 'https://www.cse.iitb.ac.in/~bhaveshd/share/6.png');
-insert into photo values (2, 'https://www.cse.iitb.ac.in/~bhaveshd/share/9.png');
+insert into photo values (1, '/images/items/1.png');
+insert into photo values (2, '/images/items/2.png');
+insert into photo values (3, '/images/items/3.png');
+insert into photo values (4, '/images/items/4.png');
+insert into photo values (4, '/images/items/5.png');
 
 insert into item_review values (1, '160050050', 4, 'Interesting Book', 'It is a really interesting book, a must read for all. Deducting one star for no bookmark!!');
 
